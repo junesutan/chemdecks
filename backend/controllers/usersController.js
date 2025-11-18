558d373f4cb46231aa162b08da49f8d1e224fc9e
junesutan <juneowns@gmail.com>
2025-11-18 16:05:13 +0800

    implemented  user registration with bcrypt and PostgreSQL

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -9,7 +9,7 @@ exports.registerUser = async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, 10);
 
     const result = await pool.query(
-      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email",
+      "INSERT INTO users (name, email, password_hash) VALUES ($1, $2, $3) RETURNING *",
       [name, email, hashedPassword]
     );
 
